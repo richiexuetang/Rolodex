9e3c02363713776a4bf3ad3348c041434c2b0ac8
Richard Tang <rxtang@ucsd.edu>
2020-08-26 13:40:28 -0700

    Updates

---
+++ precache-manifest.b076802b358549b2ad7b2eb14cd983ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0894a50d64f939fbfc52ea7857138d0",
+    "url": "/Rolodex/index.html"
+  },
+  {
+    "revision": "68e4de346b8d2a338d45",
+    "url": "/Rolodex/static/css/main.bf5c9411.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e4de346b8d2a338d45",
+    "url": "/Rolodex/static/js/main.6da05e7b.chunk.js"
+  },
+  {
+    "revision": "588df4b987e9385974e8",
+    "url": "/Rolodex/static/js/runtime-main.9725675a.js"
+  }
+]);
